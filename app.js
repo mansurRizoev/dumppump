e1d15ca7f7db9d29aae31a7faa16c76167d24010
Mansur Rizoev <mansur.rizoev.m@gmail.com>
2024-06-09 20:38:17 +0500

    asd

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-let btn = document.querySelector('.btn');
-console.log(btn)
-let audio = document.querySelector('.boom');
-btn.addEventListener('click', () => {
-    console.log('clicked');
-    audio.play();
-});
 
 window.addEventListener('keydown', (e) => {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    console.log(audio)
     audio.play();
 });
+
+/////////
+let arr = [123, true, {}]
+arr[arr.length] = "Hi mr"
+arr[arr.length] = [1, 2, 3]
+console.log(arr)
+arr.push('hi mr Dev')
+console.log(arr)
+arr.unshift("first el")
+console.log(arr)
+arr.pop()
+console.log(arr)
+arr.shift()
+console.log(arr)
+console.log(arr.includes(123))
+let arr = [1, 2, 3, 4];
+arr.forEach((el) => {
+    console.log(el * 2)
+})
+let res = arr.map((x) => x * 2)
+// console.log(arr.find(x=>x>5))
+let res3 = arr.concat([5, 6, 7, 8, 9])
+
+console.log(res3)
